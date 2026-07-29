7bc27de6b224d31260e5260e952575af977e1db8
Daniel Ruiz <djruiz44@gmail.com>
2026-07-28 20:32:31 -0400

    python crash course exercises from chapter 2

---
+++ python_crash_course/chapter_02/personal_message.py
@@ -0,0 +1,16 @@
+name = "mr Daniel ruiz"
+print(f'{name}, are you ready to learn some python today?')
+
+print("Ok, lets work on some printing functions")
+print("lowercase")
+print(name.lower())
+
+print("Uppdercase")
+print(name.upper())
+
+print("Title Case")
+print(name.title())
+
+famous_person = "Albert Einstein"
+quote = "\"A person who never made a mistake never tried anything new.\""
+print(f'\t{famous_person} once said, {quote}')
---
+++ python_crash_course/chapter_02/stripping_names.py
@@ -0,0 +1,13 @@
+name = "\t\n         Daniel Ruiz" 
+name.rstrip()
+print(name.lstrip())
+
+name = "asdfasdf           "
+print(name)
+print(name.rstrip())
+print(name)
+
+
+name = "        final test           "
+print(name)
+print(name.strip())
